ca892277c938e4e7bc9b4578a68f7c285a56d989
2lshahat''git config --global user.name 2lshahat <abosalem046@gmail.com>
2024-07-08 05:59:25 +0300

    sec commit

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const authcontext = createContext();
 
 export function Authprovider({ children }) {
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState(localStorage.getItem("token"));
 
   return (
     <authcontext.Provider value={{ token, setToken }}>
